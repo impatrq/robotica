c07595eddc7c40f70930822feba01f6283f0a8fb
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2022-07-06 15:25:03 -0300

    agrego biblioteca de motor

--- template/main.c
+++ template/main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-
-/* Defino pines para usar de trigger y echo */
-#define TRIGGER 7
-#define ECHO    8
+#include "hardware/pwm.h"
 
 /* Funciones para infrarojo */
 
@@ -27,6 +24,13 @@ static inline bool infrarojo_get(uint8_t pin) { return !gpio_get(pin); }
 void ultrasonico_init(uint8_t trigger, uint8_t echo);
 float ultrasonico_get_distance_cm(uint8_t trigger, uint8_t echo);
 
+/* Prototipos para funciones de motores */
+void motor_init(uint8_t pin_forward, uint8_t pin_reverse);
+void motor_forward(uint8_t pin_forward, uint8_t pin_reverse);
+void motor_reverse(uint8_t pin_forward, uint8_t pin_reverse);
+void motor_forward_partial(uint8_t pin_forward, uint8_t pin_reverse, uint8_t partial);
+void motor_reverse_partial(uint8_t pin_forward, uint8_t pin_reverse, uint8_t partial);
+
 /*
  *  @brief  Programa principal
  */
@@ -34,16 +38,8 @@ int main(void) {
 
     stdio_init_all();
 
-    /* Inicializacion del ultrasonico */
-    ultrasonico_init(TRIGGER, ECHO);
-
     while(1) {
-        /* Obtengo la distancia en cm */
-        float distance = ultrasonico_get_distance_cm(TRIGGER, ECHO);
-        /* Imprimo el valor en consola */
-        printf("Distancia: %.2f cm\r\n", distance);
-        /* Demora */
-        sleep_ms(1000);
+
     }
 }
 
@@ -93,4 +89,80 @@ float ultrasonico_get_distance_cm(uint8_t trigger, uint8_t echo) {
     int64_t pulse_width = absolute_time_diff_us(from, to);
     /* Calculo la distancia y la devuelvo */
     return pulse_width / 59.0;
+}
+
+/*
+ *  @brief  Inicializa los pines para trabajar con un motor de DC
+ *  @param  pin_forwart: numero de pin que controla la direccion de avance
+ *  @param  pin_reverse: numero de pin que controla la direccion de reversa
+ */
+void motor_init(uint8_t pin_forward, uint8_t pin_reverse) {
+    /* Seteo pines como PWM */
+    gpio_set_function(pin_forward, GPIO_FUNC_PWM);
+    gpio_set_function(pin_reverse, GPIO_FUNC_PWM);
+    /* Consigo los slices de los pines */
+    uint slice_pin_forward = pwm_gpio_to_slice_num(pin_forward);
+    uint slice_pin_reverse = pwm_gpio_to_slice_num(pin_forward);
+    /* Obtengo configuracion por defecto de PWM */
+    pwm_config config = pwm_get_default_config();
+    /* Inicializo ambos slices */
+    pwm_init(slice_pin_forward, &config, true);
+    pwm_init(slice_pin_reverse, &config, true);
+    /* Arranco con 0% de ancho de pulso */
+    pwm_set_gpio_level(pin_forward, 0);
+    pwm_set_gpio_level(pin_reverse, 0);
+}
+
+/*
+ *  @brief  Indica al motor de que vaya hacia adelante
+ *  @param  pin_forwart: numero de pin que controla la direccion de avance
+ *  @param  pin_reverse: numero de pin que controla la direccion de reversa
+ */
+void motor_forward(uint8_t pin_forward, uint8_t pin_reverse) {
+    /* 0% de ancho de pulso para el pin de reversa */
+    pwm_set_gpio_level(pin_reverse, 0);
+    /* 100% de ancho de pulso para el pin de avance */
+    pwm_set_gpio_level(pin_forward, 0xffff);
+}
+
+/*
+ *  @brief  Indica al motor de que vaya hacia atras
+ *  @param  pin_forwart: numero de pin que controla la direccion de avance
+ *  @param  pin_reverse: numero de pin que controla la direccion de reversa
+ */
+void motor_reverse(uint8_t pin_forward, uint8_t pin_reverse) {
+    /* 0% de ancho de pulso para el pin de avance */
+    pwm_set_gpio_level(pin_forward, 0);
+    /* 100% de ancho de pulso para el pin de reversa */
+    pwm_set_gpio_level(pin_reverse, 0xffff);
+}
+
+/*
+ *  @brief  Indica al motor de que vaya hacia adelante parcialmente
+ *  @param  pin_forwart: numero de pin que controla la direccion de avance
+ *  @param  pin_reverse: numero de pin que controla la direccion de reversa
+ *  @param  partial: porcentaje de potencia entregada
+ */
+void motor_forward_partial(uint8_t pin_forward, uint8_t pin_reverse, uint8_t partial) {
+    /* 0% de ancho de pulso para el pin de reversa */
+    pwm_set_gpio_level(pin_reverse, 0);
+    /* Calculo el valor de duty necesario */
+    uint16_t duty = (uint32_t)partial * 0xffff / 100.0;
+    /* Porcentaje de ancho de pulso indicado para el pin de avance */
+    pwm_set_gpio_level(pin_forward, duty);
+}
+
+/*
+ *  @brief  Indica al motor de que vaya hacia atras parcialmente
+ *  @param  pin_forwart: numero de pin que controla la direccion de avance
+ *  @param  pin_reverse: numero de pin que controla la direccion de reversa
+ *  @param  partial: porcentaje de potencia entregada
+ */
+void motor_reverse_partial(uint8_t pin_forward, uint8_t pin_reverse, uint8_t partial) {
+    /* 0% de ancho de pulso para el pin de avance */
+    pwm_set_gpio_level(pin_forward, 0);
+    /* Calculo el valor de duty necesario */
+    uint16_t duty = (uint32_t)partial * 0xffff / 100.0;
+    /* Porcentaje de ancho de pulso indicado para el pin de reversa */
+    pwm_set_gpio_level(pin_reverse, duty);
 }
