8eee7e80fd26da47e19551c85f2d484365145593
Juan Montilla <75455653+Monti3@users.noreply.github.com>
2022-08-20 12:53:27 -0300

    add bt library to support serial communications

--- template/libs/inc/bt.h
+++ template/libs/inc/bt.h
@@ -3,8 +3,10 @@
 
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
+/*defino que bus de uart voy a inicializar */
 #define uart_id uart0
 
+/* protitipo para funcion serial*/
 void uart_start(uint8_t tx, uint8_t rx);
 
 #endif  /* _BT_H_ */
