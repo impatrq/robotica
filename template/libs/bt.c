0f8e2aa01c6608f098a85762c9736dc49a91d378
Juan Montilla <75455653+Monti3@users.noreply.github.com>
2022-08-20 12:59:20 -0300

    add bt library to support serial communications

--- template/libs/bt.c
+++ template/libs/bt.c
@@ -1,10 +1,17 @@
 #include "bt.h"
 
+ /*
+ *  @param  tx: numero del pin tx de la pico 
+ *  @param  rx: numero del pin rx de la pico
+ */
 
-void uart_start( uint8_t tx, uint8_t rx){
+void uart_start(uint8_t tx, uint8_t rx){
 
-    uart_init(uart_id, 9600);
-    gpio_set_function(tx,GPIO_FUNC_UART);
+
+    /*incio el bus UART a 9600 baudios */
+    uart_init(uart_id, 9600);   
+    /*seteo pines como UART */
+    gpio_set_function(tx,GPIO_FUNC_UART); 
     gpio_set_function(rx, GPIO_FUNC_UART);
     
 }
